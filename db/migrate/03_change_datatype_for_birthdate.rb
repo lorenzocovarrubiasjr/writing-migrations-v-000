4e58aab0e99e7b6791c57c42517a61b2eefae831
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-10-17 19:35:29 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,5 @@
 class ChangeDataTypeForBirthdate < ActiveRecord::Migration[5.1]
   
-  def change_column(students, :birthdate, datetime) 
-    
-  end 
+  def change_column(:students, :birthdate, :datetime) 
   
 end 
